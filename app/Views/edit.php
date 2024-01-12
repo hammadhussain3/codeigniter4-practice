06028b3ed4a96956539ac3987d5a50743f428919
hammadhussain3 <dev.hammad@yahoo.com>
2024-01-12 20:32:07 +0500

    frontend design setting

--- app/Views/edit.php
+++ app/Views/edit.php
@@ -11,7 +11,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Edit User</title>
 </head>
 <body>
     <div class="container mt-3">
